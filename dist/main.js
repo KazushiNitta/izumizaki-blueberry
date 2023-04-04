c73ffe626fe7de296a56479411071af57181ec83
KazushiNitta <kazushi.sg.gr@gmail.com>
2023-04-04 16:27:05 +0900

    create price

--- dist/main.js
+++ dist/main.js
@@ -110,6 +110,114 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 
 /***/ }),
 
+/***/ "./src/assets/images/bg_paper.jpg":
+/*!****************************************!*\
+  !*** ./src/assets/images/bg_paper.jpg ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/bg_paper.jpg\");\n\n//# sourceURL=webpack:///./src/assets/images/bg_paper.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/icon_fee_box.svg":
+/*!********************************************!*\
+  !*** ./src/assets/images/icon_fee_box.svg ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/icon_fee_box.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/icon_fee_box.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/icon_fee_calender.svg":
+/*!*************************************************!*\
+  !*** ./src/assets/images/icon_fee_calender.svg ***!
+  \*************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/icon_fee_calender.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/icon_fee_calender.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/icon_fee_shop.svg":
+/*!*********************************************!*\
+  !*** ./src/assets/images/icon_fee_shop.svg ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/icon_fee_shop.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/icon_fee_shop.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/icon_fee_wallet.svg":
+/*!***********************************************!*\
+  !*** ./src/assets/images/icon_fee_wallet.svg ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/icon_fee_wallet.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/icon_fee_wallet.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/icon_time.svg":
+/*!*****************************************!*\
+  !*** ./src/assets/images/icon_time.svg ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/icon_time.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/icon_time.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/image_info_berry_01.jpg":
+/*!***************************************************!*\
+  !*** ./src/assets/images/image_info_berry_01.jpg ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/image_info_berry_01.jpg\");\n\n//# sourceURL=webpack:///./src/assets/images/image_info_berry_01.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/image_info_berry_02.jpg":
+/*!***************************************************!*\
+  !*** ./src/assets/images/image_info_berry_02.jpg ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/image_info_berry_02.jpg\");\n\n//# sourceURL=webpack:///./src/assets/images/image_info_berry_02.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/image_info_berry_03.jpg":
+/*!***************************************************!*\
+  !*** ./src/assets/images/image_info_berry_03.jpg ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/image_info_berry_03.jpg\");\n\n//# sourceURL=webpack:///./src/assets/images/image_info_berry_03.jpg?");
+
+/***/ }),
+
 /***/ "./src/assets/images/image_overview.jpg":
 /*!**********************************************!*\
   !*** ./src/assets/images/image_overview.jpg ***!
@@ -119,6 +227,30 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/image_overview.jpg\");\n\n//# sourceURL=webpack:///./src/assets/images/image_overview.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/img_calender.png":
+/*!********************************************!*\
+  !*** ./src/assets/images/img_calender.png ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/img_calender.png\");\n\n//# sourceURL=webpack:///./src/assets/images/img_calender.png?");
+
+/***/ }),
+
+/***/ "./src/assets/images/text_fee.svg":
+/*!****************************************!*\
+  !*** ./src/assets/images/text_fee.svg ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/text_fee.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/text_fee.svg?");
 
 /***/ }),
 
@@ -143,6 +275,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/text_header_sub_pc.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/text_header_sub_pc.svg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/text_info.svg":
+/*!*****************************************!*\
+  !*** ./src/assets/images/text_info.svg ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"images/text_info.svg\");\n\n//# sourceURL=webpack:///./src/assets/images/text_info.svg?");
 
 /***/ }),
 
@@ -237,7 +381,7 @@ eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./assets/stylesheets/app.scss */ \"./src/assets/stylesheets/app.scss\");\n/* harmony import */ var _assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _assets_images_bg_header_pc_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./assets/images/bg_header_pc.png */ \"./src/assets/images/bg_header_pc.png\");\n/* harmony import */ var _assets_images_text_header_sub_pc_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./assets/images/text_header_sub_pc.svg */ \"./src/assets/images/text_header_sub_pc.svg\");\n/* harmony import */ var _assets_images_text_header_guide_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./assets/images/text_header_guide.svg */ \"./src/assets/images/text_header_guide.svg\");\n/* harmony import */ var _assets_images_text_menu_top_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./assets/images/text_menu_top.png */ \"./src/assets/images/text_menu_top.png\");\n/* harmony import */ var _assets_images_text_menu_overview_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./assets/images/text_menu_overview.png */ \"./src/assets/images/text_menu_overview.png\");\n/* harmony import */ var _assets_images_text_menu_info_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./assets/images/text_menu_info.png */ \"./src/assets/images/text_menu_info.png\");\n/* harmony import */ var _assets_images_text_menu_fee_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./assets/images/text_menu_fee.png */ \"./src/assets/images/text_menu_fee.png\");\n/* harmony import */ var _assets_images_text_menu_voice_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./assets/images/text_menu_voice.png */ \"./src/assets/images/text_menu_voice.png\");\n/* harmony import */ var _assets_images_text_overview_svg__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./assets/images/text_overview.svg */ \"./src/assets/images/text_overview.svg\");\n/* harmony import */ var _assets_images_image_overview_jpg__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./assets/images/image_overview.jpg */ \"./src/assets/images/image_overview.jpg\");\n/* harmony import */ var _assets_images_bg_overview_pc_jpg__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./assets/images/bg_overview_pc.jpg */ \"./src/assets/images/bg_overview_pc.jpg\");\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./assets/stylesheets/app.scss */ \"./src/assets/stylesheets/app.scss\");\n/* harmony import */ var _assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_assets_stylesheets_app_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _assets_images_bg_header_pc_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./assets/images/bg_header_pc.png */ \"./src/assets/images/bg_header_pc.png\");\n/* harmony import */ var _assets_images_text_header_sub_pc_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./assets/images/text_header_sub_pc.svg */ \"./src/assets/images/text_header_sub_pc.svg\");\n/* harmony import */ var _assets_images_text_header_guide_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./assets/images/text_header_guide.svg */ \"./src/assets/images/text_header_guide.svg\");\n/* harmony import */ var _assets_images_text_menu_top_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./assets/images/text_menu_top.png */ \"./src/assets/images/text_menu_top.png\");\n/* harmony import */ var _assets_images_text_menu_overview_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./assets/images/text_menu_overview.png */ \"./src/assets/images/text_menu_overview.png\");\n/* harmony import */ var _assets_images_text_menu_info_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./assets/images/text_menu_info.png */ \"./src/assets/images/text_menu_info.png\");\n/* harmony import */ var _assets_images_text_menu_fee_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./assets/images/text_menu_fee.png */ \"./src/assets/images/text_menu_fee.png\");\n/* harmony import */ var _assets_images_text_menu_voice_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./assets/images/text_menu_voice.png */ \"./src/assets/images/text_menu_voice.png\");\n/* harmony import */ var _assets_images_text_overview_svg__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./assets/images/text_overview.svg */ \"./src/assets/images/text_overview.svg\");\n/* harmony import */ var _assets_images_image_overview_jpg__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./assets/images/image_overview.jpg */ \"./src/assets/images/image_overview.jpg\");\n/* harmony import */ var _assets_images_bg_overview_pc_jpg__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./assets/images/bg_overview_pc.jpg */ \"./src/assets/images/bg_overview_pc.jpg\");\n/* harmony import */ var _assets_images_text_info_svg__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./assets/images/text_info.svg */ \"./src/assets/images/text_info.svg\");\n/* harmony import */ var _assets_images_image_info_berry_01_jpg__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./assets/images/image_info_berry_01.jpg */ \"./src/assets/images/image_info_berry_01.jpg\");\n/* harmony import */ var _assets_images_image_info_berry_02_jpg__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./assets/images/image_info_berry_02.jpg */ \"./src/assets/images/image_info_berry_02.jpg\");\n/* harmony import */ var _assets_images_image_info_berry_03_jpg__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ./assets/images/image_info_berry_03.jpg */ \"./src/assets/images/image_info_berry_03.jpg\");\n/* harmony import */ var _assets_images_bg_paper_jpg__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ./assets/images/bg_paper.jpg */ \"./src/assets/images/bg_paper.jpg\");\n/* harmony import */ var _assets_images_text_fee_svg__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! ./assets/images/text_fee.svg */ \"./src/assets/images/text_fee.svg\");\n/* harmony import */ var _assets_images_icon_fee_calender_svg__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(/*! ./assets/images/icon_fee_calender.svg */ \"./src/assets/images/icon_fee_calender.svg\");\n/* harmony import */ var _assets_images_img_calender_png__WEBPACK_IMPORTED_MODULE_19__ = __webpack_require__(/*! ./assets/images/img_calender.png */ \"./src/assets/images/img_calender.png\");\n/* harmony import */ var _assets_images_icon_time_svg__WEBPACK_IMPORTED_MODULE_20__ = __webpack_require__(/*! ./assets/images/icon_time.svg */ \"./src/assets/images/icon_time.svg\");\n/* harmony import */ var _assets_images_icon_fee_wallet_svg__WEBPACK_IMPORTED_MODULE_21__ = __webpack_require__(/*! ./assets/images/icon_fee_wallet.svg */ \"./src/assets/images/icon_fee_wallet.svg\");\n/* harmony import */ var _assets_images_icon_fee_shop_svg__WEBPACK_IMPORTED_MODULE_22__ = __webpack_require__(/*! ./assets/images/icon_fee_shop.svg */ \"./src/assets/images/icon_fee_shop.svg\");\n/* harmony import */ var _assets_images_icon_fee_box_svg__WEBPACK_IMPORTED_MODULE_23__ = __webpack_require__(/*! ./assets/images/icon_fee_box.svg */ \"./src/assets/images/icon_fee_box.svg\");\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
